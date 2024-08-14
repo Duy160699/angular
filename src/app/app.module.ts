e9a2b29b5ff6fe39e417896fc7c0dd57c255febf
Duy160699 <vuduy231109@gmail.com>
2024-08-14 22:06:49 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule, TemplateRef } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AgGridAngular } from 'ag-grid-angular';
+//import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { CellCustomComponent } from './cell-custom/cell-custom.component';
+import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CellCustomComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //HttpModule,
+    HttpClientModule,
+    AgGridAngular,
+    BrowserAnimationsModule,
+    ModalModule.forRoot()
   ],
   providers: [
     provideClientHydration()
